324322651af65aa4e852f6dc53793270ce9366fe
defunsm <sxh8083@g.rit.edu>
2017-02-04 02:32:37 -0500

    new setting up files

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to finance-clj
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ src/finance_clj/core.clj
@@ -0,0 +1,129 @@
+(ns finance-clj.core
+  (:use [incanter.core :only [view]]
+        [incanter.charts :only [xy-plot add-lines scatter-plot]])
+  (:require [clj-time.core :as tc]
+            [clj-time.periodic :as tp]
+            [clj-time.coerce :as tco]))
+
+(take 25 (filter (fn [x] (>= x 12))
+                 (repeatedly (fn [] (rand 35)))))
+
+(defn generate-prices [lower-bound upper-bound]
+  (filter (fn [x] (>= x lower-bound))
+          (repeatedly (fn [] (rand upper-bound)))))
+
+(def y (take 50 (generate-prices 10 35)))
+(def x (into [] (range (count y))))
+(view (xy-plot x y))
+
+(def pricelist (generate-prices 12 35))
+(take 25 (map (fn [x] {:price x}) pricelist))
+
+(take 25 (map (fn [x y]
+                [x y])
+              (map (fn [x] {:time x}) (iterate inc 0))
+              (map (fn [x] {:price x}) pricelist)))
+
+(take 25 (->> (map (fn [x y]
+                     [x y])
+                   (map (fn [x] {:time x}) (iterate inc 0))
+                   (map (fn [x] {:price x}) pricelist))
+              (map (fn [x] (merge (first x) (second x))))))
+
+(defn generate-timeseries [pricelist]
+  (map (fn [x y]
+         {:time x :price y})
+       (iterate inc 0)
+       pricelist))
+
+(def set (take 25 (generate-timeseries pricelist)))
+
+(view (xy-plot (map :time set)
+               (map :price set)))
+
+;; Chapter 2
+
+(defn random-in-range [lower upper]
+  (let [r (rand upper)]
+    (if (>= r lower)
+      r
+      (+ (rand (- upper lower))
+         lower))))
+
+(defn stochastic-k [last-price low-price high-price]
+  (let [hlrange (- high-price low-price)
+        hlmidpoint (/ hlrange 2)
+        numerator (if (> last-price hlmidpoint)
+                    (- last-price hlmidpoint)
+                    (- hlmidpoint low-price))]
+    (/ numerator hlrange)))
+
+(defn break-local-minima-maxima [k]
+  (as-> k k
+    (if (<= (int (+ 0.95 k)) 0)
+      (+ 0.15 k) k)
+    (if (>= k 1)
+      (- k 0.15) k)))
+
+(defn generate-prices
+  ([low high]
+   (generate-prices (random-in-range low high)))
+  ([last-price]
+   (iterate (fn [{:keys [last]}]
+              (let [low (- last 5)
+                    high (+ last 5)
+                    k (stochastic-k last low high)
+                    plus-OR-minus (rand-nth [- +])
+                    kpm (if (= plus-OR-minus +)
+                          (+ 1 (break-local-minima-maxima k))
+                          (- 1 (break-local-minima-maxima k)))
+                    newprice (* kpm last)
+                    newlow (if (< newprice low) newprice low)
+                    newhigh (if (> newprice high) newprice high)]
+                {:last newprice}))
+            {:last last-price})))
+
+(defn generate-timeseries
+  ([pricelist]
+   (generate-timeseries pricelist (tc/now)))
+  ([pricelist datetime]
+   (->> (map (fn [x y] [x y])
+             (map (fn [x] {:time x}) (iterate #(tc/plus % (tc/seconds (rand 4))) datetime))
+             (map (fn [x] {:price x}) pricelist))
+        (map (fn [x] (merge (first x) (second x)))))))
+
+(def pricelist (generate-prices 5 15))
+
+(def prices (map :last (map :price (take 40 (generate-timeseries pricelist)))))
+
+(def num (count prices))
+
+(defn moving-avg [n]
+  (/ (reduce + (first (split-at (+ n 1) prices))) (+ n 1)))
+
+(defn create-moving-avg []
+  (loop [n 0 acc []]
+    (if (> n (count prices))
+      acc
+      (recur (inc n) (conj acc (moving-avg n))))))
+
+(def mv-avg (create-moving-avg))
+
+(defn create-graph [lower-bound upper-bound]
+  (let [pricelist (generate-prices lower-bound upper-bound)
+        prices (map :last (map :price (take 40 (generate-timeseries pricelist))))
+        mv-avg (create-moving-avg)]
+    (view (add-lines (xy-plot (range (count prices)) prices)
+                     (range (count prices)) mv-avg))))
+
+(create-graph 5 15)
+
+
+
+
+
+(conj [2 1] 1)
+
+(view (add))
+
+(random-in-range 10 20)
