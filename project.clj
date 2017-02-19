c25819cb92ceae24cfab2ab6e7e118d118a84cef
defunsm <sxh8083@g.rit.edu>
2017-02-18 23:24:38 -0500

    trying to fix build 2

--- project.clj
+++ project.clj
@@ -10,15 +10,24 @@
                  [ring/ring-jetty-adapter "1.5.1"]
                  [compojure "1.5.2"]
                  [hiccup "1.0.5"]
-                 [org.clojure/clojurescript "1.9.473"]]
+                 [org.clojure/clojurescript "1.9.473"]
+                 [environ "1.0.0"]
+                 [compojure "1.4.0"]]
   :main finance-clj.web
+  :min-lein-version "2.0.0"
   :plugins [[lein-ring "0.11.0"]
-            [lein-cljsbuild "1.1.5"]]
-  :ring {:handler finance-clj.analytics/app}
+            [lein-cljsbuild "1.1.5"]
+            [environ/environ.lein "0.3.1"]]
+  :hooks [environ.leiningen.hooks]
+  :ring {:handler finance-clj.web/app}
 
   :cljsbuild {:builds
               [{:source-paths ["src-cljs"],
                 :compiler
                 {:pretty-print true,
                  :output-to "resources/js/script.js",
-                 :optimizations :whitespace}}]})
+                 :optimizations :whitespace}}]}
+
+  :uberjar-name "clojure-getting-started-standalone.jar"
+
+  :profiles {:production {:env {:production true}}}))
